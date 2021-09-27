88d7e356fadd27a6c4c3cb7d9831c1e6c790156b
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2021-09-27 15:35:59 +0200

    Add project files.

---
+++ Dojo_01_CSV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dojo_01_CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
