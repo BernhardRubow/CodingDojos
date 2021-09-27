61dacb8e05dc2aec4c6e27c16740bce11b2bc624
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2021-09-27 17:01:22 +0200

    First Working Solution

--- Dojo_01_CSV/Program.cs
+++ Dojo_01_CSV/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace Dojo_01_CSV
 {
     class Program
     {
+        private static string[,] colummnValues = null;
+        private static int[] columnWidths = null;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var CSV_zeilen = @"
+Name;Strasse;Ort;Alter
+Peter Pan;Am Hang 5;12345 Einsam;42
+Maria Schmitz;Kölner Straße 45;50123 Köln;43
+Paul Meier;Münchener Weg 1;87654 München;65
+".SplitByString(Environment.NewLine);
+
+            var result = Tabelliere(CSV_zeilen);
+
+            foreach (var row in result)
+            {
+                Console.WriteLine(row);
+            }
+
+            
+        }
+
+        public static IEnumerable<string> Tabelliere(IEnumerable<string> CSV_zeilen)
+        {
+            var rows = CSV_zeilen.ToArray();
+            
+            for (int i = 0; i < rows.Length; i++)
+            {
+                var cols = rows[i].SplitByString(";").ToArray();
+
+                colummnValues ??= new string[rows.Length, cols.Length];
+                columnWidths ??= new int[cols.Length];
+
+                for (int j = 0; j < cols.Length; j++)
+                {
+                    colummnValues[i, j] = cols[j];
+                    if (columnWidths[j] < cols[j].Length) columnWidths[j] = cols[j].Length;
+                }
+            }
+
+            List<string> result = new List<string>();
+            for (int i = 0; i < rows.Length; i++)
+            {
+                string line = "|";
+                for (int j = 0; j < columnWidths.Length; j++)
+                {
+                    line += colummnValues[i, j].PadRight(columnWidths[j]) + "|";
+                }
+                result.Add(line);
+            }
+
+
+
+            return result;
+        }
+
+
+
+    }
+
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<string> SplitByString(this string s, string splitString)
+        {
+            string[] values = s.Split(new[] { splitString }, StringSplitOptions.RemoveEmptyEntries);
+
+            return values.ToList();
         }
     }
 }
